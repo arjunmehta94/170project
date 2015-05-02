338586a552a2f81358fe7ab8c3a3f3c769007df0
snoozzzenbob <sanketq@berkeley.edu>
2015-05-02 13:08:06 -0700

    Fixed Error

--- phase1/3increator.py
+++ phase1/3increator.py
@@ -1,16 +1,19 @@
 nodes = 48
 group = 0
 changed = 1
+first = True
 for y in range(nodes):
 	for x in range(nodes):
 		if changed == 4:
-			if((((group + 1) * 4) - 1) <= x <= (((group + 1) * 4) + 1)):
+			if((group*4) + 2 <= x <= (group*4) + 4):
 				print '0',
 			else:
 				print '100',
-		elif (group * 4) <= x <= (((group + 1) * 4) - 1):
+		elif (group * 4) <= x <= (group*4) + 2:
 			print '0',
-		elif changed == 3 and x == ((group + 1) * 4):
+		elif changed == 3 and x == (group*4) + 3:
+			print '0',
+		elif changed == 1 and x == (group * 4) - 1:
 			print '0',
 		else: 
 			print '100',
@@ -18,6 +21,8 @@ for y in range(nodes):
 	if changed == 4:
 		changed = 1
 		group += 1
+		if first:
+			first = False
 	else:
 		changed += 1
 
