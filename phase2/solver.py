d6b4765a6a410a0acc0ce69a4db20454b14228c8
Arjun Mehta <arjunmehta94@berkeley.edu>
2015-05-08 13:37:56 -0700

    solver.py last char error

--- phase2/solver.py
+++ phase2/solver.py
@@ -7,6 +7,7 @@ T = 1 # number of test cases
 fout = open ("answer-test.out", "w")
 for t in xrange(1, T+1):
 	#print t
+	doubleDigit = False
 	fin = open("instances/240.in", "r")# + str(t) + ".in", "r")
 	#fin = open("instances/" + str(t) + ".in", "r")
 	N = int(fin.readline())
@@ -17,9 +18,16 @@ for t in xrange(1, T+1):
 	#print d
 	tsp = AntTSP(N,d,c)
 	assign = tsp.findSolution()
+	if assign[-1] < 10:
+		doubleDigit = True
+	else:
+		doubleDigit = False
 	assign = str(assign).split(', ')
 	assign[0] = assign[0][1:]
-	assign[-1] = assign[-1][0:1]
+	if doubleDigit:
+		assign[-1] = assign[-1][0:1]
+	else:
+		assign[-1] = assign[-1][0:2]
 	# find an answer, and put into assign
 	# assign = [0] * N
 	# for i in xrange(N):
