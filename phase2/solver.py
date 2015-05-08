a71d1035d1d437758c9928d05b7e565c4f0919ac
Tom Meng <TomMeng@Lingxuans-MacBook-Pro.local>
2015-05-08 13:42:48 -0700

    almost

--- phase2/solver.py
+++ phase2/solver.py
@@ -3,9 +3,9 @@ from antcolony import AntColony
 from antgraph import AntGraph
 from ant import Ant
 
-T = 495 # number of test cases
-fout = open ("answer401-495.out", "w")
-for t in xrange(401, T+1):
+T = 10 # number of test cases
+fout = open ("answer10.out", "w")
+for t in xrange(1, T+1):
 	print t
 	#fin = open("instances/492.in", "r")# + str(t) + ".in", "r")
 	fin = open("instances/" + str(t) + ".in", "r")
@@ -16,9 +16,13 @@ for t in xrange(401, T+1):
 	c = fin.readline()
 	#print d
 	tsp = AntTSP(N,d,c)
-	assign = tsp.findSolution()
-	assign = str(assign).split(', ')
+	intArray = tsp.findSolution()
+	assign = str(intArray).split(', ')
 	assign[0] = assign[0][1:]
+	if intArray[-1] < 10:
+		assign[-1] = assign[-1][0:1]
+	else:
+		assign[-1] = assign[-1][0:2]
 	assign[-1] = assign[-1][0:2] ############ was: assign[-1] = assign[-1][0:1]
 	# find an answer, and put into assign
 	# assign = [0] * N
