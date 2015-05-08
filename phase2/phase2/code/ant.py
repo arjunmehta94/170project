85cb2a8eb33a3fcea55a9aabe4c00baed7c90c25
Arjun Mehta <arjunmehta94@berkeley.edu>
2015-05-08 15:04:44 -0700

    Final writeup added, final submission in phase2 dir

---
+++ phase2/phase2/code/ant.py
@@ -0,0 +1,174 @@
+import math
+import random
+import sys
+from threading import *
+
+class Ant(Thread):
+    def __init__(self, ID, start_node, colony):
+        Thread.__init__(self)
+        self.ID = ID
+        self.start_node = start_node
+        self.colony = colony
+
+        self.curr_node = self.start_node
+        self.graph = self.colony.graph
+        self.path_vec = []
+        self.path_vec.append(self.start_node)
+        self.path_cost = 0
+
+        self.last1 = None
+        self.last2 = start_node###########################
+        # same meaning as in standard equations
+        self.Beta = 1
+        #self.Q0 = 1  # Q0 = 1 works just fine for 10 city case (no explore)
+        self.Q0 = 0.5
+        self.Rho = 0.99
+
+        # store the nodes remaining to be explored here
+        self.nodes_to_visit = {}
+        self.nodes_to_visit_blue = {}
+        self.nodes_to_visit_red = {}
+
+
+        for i in range(0, self.graph.num_nodes):
+            if i != self.start_node:
+                if self.graph.color(i) == "B":
+                    self.nodes_to_visit_blue[i] = i
+                else:
+                    self.nodes_to_visit_red[i] = i
+                self.nodes_to_visit[i] = i
+
+        # create n X n matrix 0'd out to start
+        self.path_mat = []
+
+        for i in range(0, self.graph.num_nodes):
+            self.path_mat.append([0]*self.graph.num_nodes)
+
+    # overide Thread's run()
+    def run(self):
+        graph = self.colony.graph
+        while not self.end():
+            # we need exclusive access to the graph
+            graph.lock.acquire()
+            new_node = self.state_transition_rule(self.curr_node)
+            print "Ant %s color %s node %s" % (self.ID, self.graph.color(new_node), new_node,)
+            self.path_cost += graph.delta(self.curr_node, new_node)
+
+            self.path_vec.append(new_node)
+            self.path_mat[self.curr_node][new_node] = 1  #adjacency matrix representing path
+
+            self.local_updating_rule(self.curr_node, new_node)
+            graph.lock.release()
+
+            self.curr_node = new_node
+
+
+        # don't forget to close the tour
+        self.path_cost += graph.delta(self.path_vec[-1], self.path_vec[0])
+
+        # send our results to the colony
+        if self.path_cost == 0:
+            self.path_cost = 0.0000001
+        self.colony.update(self)
+        print "Ant %s : %s, %s" % (self.ID, self.path_vec, self.path_cost,)
+
+        # allows thread to be restarted (calls Thread.__init__)
+        self.__init__(self.ID, self.start_node, self.colony)
+
+    def end(self):
+        return not self.nodes_to_visit 
+
+    # described in report -- determines next node to visit after curr_node
+    def state_transition_rule(self, curr_node):
+        graph = self.colony.graph
+        q = random.random()
+        max_node = -1
+
+        if self.last1 is None or self.last2 is None:
+            nodes_to_visit = self.nodes_to_visit
+        elif self.graph.color(self.last1) == "B" and self.graph.color(self.last2) == "B":
+            nodes_to_visit = self.nodes_to_visit_red
+        elif self.graph.color(self.last1) == "R" and self.graph.color(self.last2) == "R":
+            nodes_to_visit = self.nodes_to_visit_blue
+        elif len(self.nodes_to_visit) < 5:
+            if len(self.nodes_to_visit_blue)  < len(self.nodes_to_visit_red):
+                nodes_to_visit = self.nodes_to_visit_red
+            else:
+                nodes_to_visit = self.nodes_to_visit_blue
+        elif len(self.nodes_to_visit_blue) -2 == len(self.nodes_to_visit_red) // 2:
+            nodes_to_visit = self.nodes_to_visit_red
+        elif len(self.nodes_to_visit_red) -2 == len(self.nodes_to_visit_blue) // 2:
+            nodes_to_visit = self.nodes_to_visit_blue
+        else:
+            nodes_to_visit = self.nodes_to_visit
+
+        print "Blue"
+        print self.nodes_to_visit_blue
+        print "Red"
+        print self.nodes_to_visit_red
+        print "All"
+        print self.nodes_to_visit
+
+        if q < self.Q0:
+            print "Ant", self.ID, "continues on its journey!"
+            max_val = -1
+            val = None
+
+            for node in nodes_to_visit.values():
+                if graph.tau(curr_node, node) == 0:
+                    raise Exception("tau = 0")
+
+                val = graph.tau(curr_node, node) * math.pow(graph.etha(curr_node, node), self.Beta)
+                if val > max_val:
+                    max_val = val
+                    max_node = node
+        else:
+            print "Exploration"
+            sum = 0
+            node = -1
+
+            for node in nodes_to_visit.values():
+
+                if graph.tau(curr_node, node) == 0:
+                    raise Exception("tau = 0")
+                sum += graph.tau(curr_node, node) * math.pow(graph.etha(curr_node, node), self.Beta)
+            if sum == 0:
+                raise Exception("sum = 0")
+
+            avg = sum / len(self.nodes_to_visit)
+
+            print "avg = %s" % (avg,)
+
+            for node in nodes_to_visit.values():
+                p = graph.tau(curr_node, node) * math.pow(graph.etha(curr_node, node), self.Beta) 
+                if p > avg:
+                    print "p = %s" % (p,)
+                    max_node = node
+                    break
+                    
+
+            if max_node == -1:
+                max_node = node
+
+        self.last1 = self.last2
+        self.last2 = max_node
+
+        if max_node < 0:
+            raise Exception("max_node < 0")
+
+        del self.nodes_to_visit[max_node]
+        if max_node in self.nodes_to_visit_blue:
+            del self.nodes_to_visit_blue[max_node]
+        if max_node in self.nodes_to_visit_red:
+            del self.nodes_to_visit_red[max_node]
+        
+
+
+        return max_node
+
+    # phermone update rule for indiv ants
+    def local_updating_rule(self, curr_node, next_node):
+        graph = self.colony.graph
+        val = (1 - self.Rho) * graph.tau(curr_node, next_node) + (self.Rho * graph.tau0)
+        graph.update_tau(curr_node, next_node, val)
+
---
+++ phase2/phase2/code/anttsp.py
@@ -0,0 +1,77 @@
+from antcolony import AntColony
+from antgraph import AntGraph
+
+import pickle
+import sys
+import traceback
+import re
+
+class AntTSP:
+    
+    # def __init__(self, input_str):
+    def __init__(self,N,d,c):  
+        self.num_nodes = N
+        self.num_ants = 0
+        self.num_iterations = 0
+        self.num_repetitions = 0
+        self.color_mat = c
+        self.cost_mat = d
+        #matchObj = re.match("^(\d*)\n([\d\s]+)\n([a-zA-Z]+)$", input_str)
+        # p = re.compile(ur'^(\d*)\n([\d\s]+)\n([a-zA-Z]+)$')
+        # matchObj = re.search(p, unicode(input_str))
+        #print matchObj      
+        # if(matchObj):
+        #     self.num_nodes = eval(matchObj.group(1))
+        #     cost_mat_prep = matchObj.group(2)
+        #     #self.cost_mat = [[0 for x in range(self.num_nodes)] for x in range(self.num_nodes)]
+        #     self.cost_mat = cost_mat_prep.split("\n", self.num_nodes)
+        #     for i in range(0,self.num_nodes):
+        #         self.cost_mat[i] = self.cost_mat[i].split(" ")
+            
+        #     for x in range(0,self.num_nodes):
+        #         for y in range(0,self.num_nodes):
+        #             self.cost_mat[x][y] = int(self.cost_mat[x][y])
+        #             # del self.cost_mat[x][self.num_nodes]
+           
+        #     self.color_mat = matchObj.group(3)
+        # else:
+        #     print "Exception: Invalid String"
+        #     return
+
+        if self.num_nodes <= 10:
+            self.num_ants = 15
+            self.num_iterations = 12
+            self.num_repetitions = 1
+        else:
+            self.num_ants = 50
+            self.num_iterations = 20
+            self.num_repetitions = 1
+
+        if self.num_nodes < len(self.cost_mat):
+            self.cost_mat = self.cost_mat[0:self.num_nodes]
+            self.color_mat = self.color_mat[0:self.num_nodes]
+            for i in range(0, self.num_nodes):
+                self.cost_mat[i] = self.cost_mat[i][0:self.num_nodes]
+
+    def findSolution(self):
+        try:
+            graph = AntGraph(self.num_nodes, self.cost_mat, self.color_mat)
+            best_path_vec = None
+            best_path_cost = sys.maxint
+            for i in range(0, self.num_repetitions):
+                #print "blah"
+                graph.reset_tau()
+                ant_colony = AntColony(graph, self.num_ants, self.num_iterations)
+                ant_colony.start()
+                if ant_colony.best_path_cost < best_path_cost:
+                    best_path_vec = ant_colony.best_path_vec
+                    best_path_cost = ant_colony.best_path_cost
+            path = []
+            for x in best_path_vec:
+                path.append(x+1)
+            best_path_vec = path
+            return best_path_vec
+
+        except Exception, e:
+            print "exception: " + str(e)
+            traceback.print_exc()
